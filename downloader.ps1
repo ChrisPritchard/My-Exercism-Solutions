a0a2801d3db98b58d367c19d2ee604a1e1deae96
Chris Pritchard <chris.pritchard@edpay.nz>
2018-10-24 12:44:27 +1300

    Added a comment about customising for different languages

--- downloader.ps1
+++ downloader.ps1
@@ -1,3 +1,6 @@
+# to run for a given language, replace the trackname 'csharp' below with whatever is appropriate (e.g. fsharp)
+# this needs to be done in two places: line 4 in the exercise url, and line 10 in the expression invocation
+
 $request = "https://api.github.com/repos/exercism/csharp/contents/exercises"
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $result = Invoke-WebRequest $request | ConvertFrom-Json
