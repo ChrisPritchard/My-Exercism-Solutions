06b307ed0a264c90e4fd7aa6d0f6db64ebad0a8f
Christopher <chrispritchard.nz@gmail.com>
2019-03-21 19:28:57 +1300

    1 extra exercise on c# and f# tracks

---
+++ csharp/resistor-color/ResistorColor.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public static class ResistorColor
+{
+    public static int ColorCode(string color) => Array.IndexOf(Colors(), color);
+
+    public static string[] Colors() => new[] {"black","brown","red","orange","yellow","green","blue","violet","grey","white"};
+}
---
+++ fsharp/dnd-character/DndCharacter.fs
@@ -0,0 +1,28 @@
+﻿module DndCharacter
+
+open System
+
+let random = Random ()
+
+let modifier x =
+    float (x - 10) / 2. |> floor |> int
+
+let ability() = 
+    List.init 4 (fun _ -> random.Next(6) + 1)
+    |> List.skip 1 
+    |> List.sum
+
+type DndCharacter() =
+
+    let str, dex, con, int, wis, chr =
+        ability (), ability (), ability (), 
+        ability (), ability (), ability ()
+
+    member __.Strength with get() = str
+    member __.Dexterity with get() = dex
+    member __.Constitution with get() = con
+    member __.Intelligence with get() = int
+    member __.Wisdom with get() = wis
+    member __.Charisma with get() = chr
+    member __.Hitpoints with get() = modifier con + 10
+
