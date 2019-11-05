4b7c83c83f5d2d9e8682e0ea7d80be09e76dc7fa
Christopher <chrispritchard.nz@gmail.com>
2019-11-05 22:00:56 +1300

    simpler version of hamming

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,9 +1,6 @@
 def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("mismatching lengths")
-    count = 0
-    for i in range(len(strand_a)):
-        if strand_a[i] != strand_b[i]:
-            count += 1
-    return count
+    mismatches = [a for a, b in zip(strand_a, strand_b) if a != b]
+    return len(mismatches)
 
