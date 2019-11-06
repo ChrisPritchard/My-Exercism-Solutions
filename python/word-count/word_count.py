a207c8452c0bf9fd434ea22e50c84b1d1e9e274e
Christopher <chrispritchard.nz@gmail.com>
2019-11-06 19:11:50 +1300

    finished word count

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,7 +1,13 @@
 def count_words(sentence):
+    for b in ":!@.$&%^":
+        sentence = sentence.replace(b, "")
+    for w in "\n\r\t,_":
+        sentence = sentence.replace(w, " ")
+
     counts = {}
-    for word in sentence.lower().translate(None, ":!@").split():
-        if counts[word]:
+    for word in sentence.lower().split():
+        word = word.strip('\'')
+        if word in counts:
             counts[word] += 1
         else:
             counts[word] = 1
