e1d3188606a2d3571c4906a6a8c2f70da9328233
Chris Pritchard <cpritchard@Chriss-MacBook-Pro.local>
2020-05-04 14:08:30 +1200

    first version

---
+++ bash/hamming/hamming.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+if [ $# != 2 ]
+then
+    echo "Usage: hamming.sh <string1> <string2>"
+    exit 1
+elif [ ${#1} != ${#2} ]
+then
+    echo "left and right strands must be of equal length"
+    exit 1
+fi
+
+result=0
+left=$1
+right=$2
+
+for ((i=0;i<${#left};i++))
+do
+    if [ ${left:$i:1} != ${right:$i:1} ]
+    then
+        result=$((result+1))
+    fi
+done
+
+echo $result
---
+++ bash/hamming/hamming_test.sh
@@ -0,0 +1,93 @@
+#!/usr/bin/env bash
+
+# local version: 2.3.0.3
+# 
+# bash-specific test: Input validation, proper quoting
+
+@test 'empty strands' {
+  #[[ $BATS_RUN_SKIPPED == "true" ]] || skip
+  run bash hamming.sh '' ''
+  (( status == 0 ))
+  [[ $output == "0" ]]
+}
+
+@test 'single letter identical strands' {
+  [[ $BATS_RUN_SKIPPED == "true" ]] || skip
+  run bash hamming.sh 'A' 'A'
+  (( status == 0 ))
+  [[ $output == "0" ]]
+}
+
+@test 'single letter different strands' {
+  [[ $BATS_RUN_SKIPPED == "true" ]] || skip
+  run bash hamming.sh 'G' 'T'
+  (( status == 0 ))
+  [[ $output == "1" ]]
+}
+
+@test 'long identical strands' {
+  [[ $BATS_RUN_SKIPPED == "true" ]] || skip
+  run bash hamming.sh 'GGACTGAAATCTG' 'GGACTGAAATCTG'
+  (( status == 0 ))
+  [[ $output == "0" ]]
+}
+
+@test 'long different strands' {
+  [[ $BATS_RUN_SKIPPED == "true" ]] || skip
+  run bash hamming.sh 'GGACGGATTCTG' 'AGGACGGATTCT'
+  (( status == 0 ))
+  [[ $output == "9" ]]
+}
+
+@test 'disallow first strand longer' {
+  [[ $BATS_RUN_SKIPPED == "true" ]] || skip
+  run bash hamming.sh 'AATG' 'AAA'
+  (( status == 1 ))
+  [[ $output == "left and right strands must be of equal length" ]]
+}
+
+@test 'disallow second strand longer' {
+  [[ $BATS_RUN_SKIPPED == "true" ]] || skip
+  run bash hamming.sh 'ATA' 'AGTG'
+  (( status == 1 ))
+  [[ $output == "left and right strands must be of equal length" ]]
+}
+
+@test 'disallow left empty strand' {
+  [[ $BATS_RUN_SKIPPED == "true" ]] || skip
+  run bash hamming.sh '' 'G'
+  (( status == 1 ))
+  [[ $output == "left and right strands must be of equal length" ]]
+}
+
+@test 'disallow right empty strand' {
+  [[ $BATS_RUN_SKIPPED == "true" ]] || skip
+  run bash hamming.sh 'G' ''
+  (( status == 1 ))
+  [[ $output == "left and right strands must be of equal length" ]]
+}
+
+@test "no input" {
+  [[ $BATS_RUN_SKIPPED == "true" ]] || skip
+  run bash hamming.sh
+  (( status == 1 ))
+  [[ $output == "Usage: hamming.sh <string1> <string2>" ]]
+}
+
+@test "invalid input" {
+  [[ $BATS_RUN_SKIPPED == "true" ]] || skip
+  run bash hamming.sh 'A'
+  (( status == 1 ))
+  [[ $output == "Usage: hamming.sh <string1> <string2>" ]]
+}
+
+# Within [[...]] the == operator is a _pattern matching_ operator.
+# To test for string equality, the right-hand side must be
+# quoted to prevent interpretation as a glob-style pattern.
+
+@test "expose subtle '[[ \$x == \$y ]]' bug" {
+  [[ $BATS_RUN_SKIPPED == "true" ]] || skip
+  run bash hamming.sh 'AAA' 'A?A'
+  (( status == 0 ))
+  [[ $output == "1" ]]
+}
