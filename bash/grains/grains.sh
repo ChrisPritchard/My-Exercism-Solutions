e561a8e8c9792b3190a11a170321f8e1996b625d
Chris Pritchard <cpritchard@Chriss-MacBook-Pro.local>
2020-05-08 10:58:58 +1200

    finished grains

---
+++ bash/grains/grains.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if [ $1 = "total" ]
+then
+    total="0"
+    for ((n=1;n<65;n++))
+    do
+        total=$(bc <<< "$total + $(sh $0 $n)")
+    done
+    echo $total
+    exit 0
+fi
+
+[ $1 -lt 1 ] || [ $1 -gt 64 ] && echo "Error: invalid input" && exit 1
+power=$(($1-1))
+
+bc <<< "2^$power"
