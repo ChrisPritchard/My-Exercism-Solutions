6ff8521308b2d1547c6db742bc78735bf95e22a5
Christopher <chrispritchard.nz@gmail.com>
2020-05-13 21:48:56 +1200

    experiements with regex

--- bash/phone-number/phone_number.sh
+++ bash/phone-number/phone_number.sh
@@ -1,29 +1,10 @@
 #!/usr/bin/env bash
 
-error="Invalid number.  [1]NXX-NXX-XXXX N=2-9, X=0-9"
-withoutspaces=$(echo $1 | tr -d ' ')
-valid=$(echo $withoutspaces | tr -dc "0-9-+[].()")
-
-if [ ${#valid} -ne ${#withoutspaces} ]; then
-    echo "$error" && exit 1
+if [[ $1 =~ _*+?1?[_\-]*(?[2-9][0-9]{2})?[_\.\-]*[2-9][0-9]{2}[_\.\-]*[0-9]{4}\_* ]]; then
+    result=$(echo $1 | tr -dc 0-9)
+    # if [ ${#result} -eq 11 ]; then echo ${result:1:10} else echo $result; fi
+    echo $result
+    exit 0
 fi
 
-numbers=$(echo $1 | tr -dc "0-9")
-length=${#numbers}
-
-if [ $length -gt 11 ] || [ $length -lt 10 ]; then
-    echo "$error" && exit 1
-fi
-
-if [ $length -eq 11 ]; then
-    if [ ${numbers:0:1} -ne 1 ]; then
-        echo "$error" && exit 1
-    fi
-    numbers=${numbers:1:10}
-fi
-
-if [ ${numbers:0:1} -lt 2 ] || [ ${numbers:3:1} -lt 2 ]; then
-    echo "$error" && exit 1
-fi
-
-echo "$numbers"
+echo "Invalid number.  [1]NXX-NXX-XXXX N=2-9, X=0-9" && exit 1
