c2e80736b4f060410f63042c956dec5600b528bb
Chris Pritchard <cpritchard@Chriss-MacBook-Pro.local>
2020-05-14 13:45:41 +1200

    redid phone number with regex woo

--- bash/phone-number/phone_number.sh
+++ bash/phone-number/phone_number.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
-if [[ $1 =~ _*+?1?[_\-]*(?[2-9][0-9]{2})?[_\.\-]*[2-9][0-9]{2}[_\.\-]*[0-9]{4}\_* ]]; then
-    result=$(echo $1 | tr -dc 0-9)
-    # if [ ${#result} -eq 11 ]; then echo ${result:1:10} else echo $result; fi
-    echo $result
-    exit 0
-fi
+valid=$(grep "^\ *+\?1\?\ *(\?[2-9]\d\d)\?[\ \.\-]*[2-9]\d\d[\ \.\-]*\d\d\d\d *$" <<< $1)
+if [[ -z "$valid" ]]; then echo "Invalid number.  [1]NXX-NXX-XXXX N=2-9, X=0-9" && exit 1; fi
 
-echo "Invalid number.  [1]NXX-NXX-XXXX N=2-9, X=0-9" && exit 1
+result="${valid//[^0-9]}"
+if [ ${#result} -eq 11 ]; then echo ${result:1:10}; else echo $result; fi
