ab774c3c18930a3184a42fb76479f581fed04f3e
Christopher <chrispritchard.nz@gmail.com>
2020-05-17 12:34:34 +1200

    further triangle work

--- bash/triangle/triangle.sh
+++ bash/triangle/triangle.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 
-# test all sides greater than 0, and when ordered, the sum of the first to is >= the largest
-# then go by type of triangle.
-
 type=$1
-sides="$2 $3 $4"
-item() {
-    IFS=' '
-    echo "$sides" | sort -n | sed "${1}q;d"
-}
 
-echo $(item 1)
-echo $(item 2)
-echo $(item 3)
+if [ "$type" == "equilateral" ]; then
+    [ $(bc <<< "$2 > 0 && $2 == $3 && $3 == $4") -eq 1 ] && echo "true" || echo "false"
+    exit 0
+fi
+
+sides=($2 $3 $4)
+IFS=$'\n' sides=($(sort <<< "${sides[*]}")); unset IFS
+
+if [ $(bc <<< "$sides[0] > 0 && $sides[0] + $sides[1] >= $sides[2]") -eq 0 ]; then
+    echo "false"
+    exit 0
+fi
+
+if [ "$type" == "isosceles" ]; then
+    [ $(bc <<< "$sides[0] == $sides[1] || $sides[1] == $sides[2]") -eq 1 ] && echo "true" || echo "false"
+elif [ "$type" == "scalene" ]; then
+    [ $(bc <<< "$sides[0] != $sides[1] && $sides[1] != $sides[2] && $sides[0] != $sides[2]") -eq 1 ] && echo "true" || echo "false"
+fi
