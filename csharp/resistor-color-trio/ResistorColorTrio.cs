dd80073d3ed35b2806df94102483536b590b6a3a
Chris Pritchard <chris@grislygrotto.nz>
2021-08-04 21:23:33 +1200

    implemented trio

--- csharp/resistor-color-trio/ResistorColorTrio.cs
+++ csharp/resistor-color-trio/ResistorColorTrio.cs
@@ -2,8 +2,15 @@ using System;
 
 public static class ResistorColorTrio
 {
+    private static readonly string[] colours = new[] {
+        "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+
+    private static int Value(string[] colors) => 
+        (Array.IndexOf(colours, colors[0]) * 10 + Array.IndexOf(colours, colors[1]));
+
     public static string Label(string[] colors)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var val = Value(colors) * Math.Pow(10, Array.IndexOf(colours, colors[2]));
+        return $"{val} ohms".Replace("000 ohms", " kiloohms");
     }
 }
--- csharp/resistor-color-trio/ResistorColorTrioTests.cs
+++ csharp/resistor-color-trio/ResistorColorTrioTests.cs
@@ -10,25 +10,25 @@ public class ResistorColorTrioTests
         Assert.Equal("33 ohms", ResistorColorTrio.Label(new[] { "orange", "orange", "black" }));
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact]
     public void Blue_and_grey_and_brown()
     {
         Assert.Equal("680 ohms", ResistorColorTrio.Label(new[] { "blue", "grey", "brown" }));
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact]
     public void Red_and_black_and_red()
     {
         Assert.Equal("2 kiloohms", ResistorColorTrio.Label(new[] { "red", "black", "red" }));
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact]
     public void Green_and_brown_and_orange()
     {
         Assert.Equal("51 kiloohms", ResistorColorTrio.Label(new[] { "green", "brown", "orange" }));
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact]
     public void Yellow_and_violet_and_yellow()
     {
         Assert.Equal("470 kiloohms", ResistorColorTrio.Label(new[] { "yellow", "violet", "yellow" }));
