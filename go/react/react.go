6ed30d93282226aa6d133746257dc5554556cd27
Chris Pritchard <chris@grislygrotto.nz>
2021-08-11 16:31:27 +1200

    almost there

--- go/react/react.go
+++ go/react/react.go
@@ -14,85 +14,77 @@ func New() *MyReactor {
 }
 
 func (r *MyReactor) CreateInput(n int) InputCell {
-	v := MyInputCell{}
+	v := MyCell{}
 	v.value = n
-	v.onChange = func(int) {}
+	v.callbacks = []*func(int){}
 	return &v
 }
 
 func (r *MyReactor) CreateCompute1(source Cell, onChange func(int) int) ComputeCell {
-	v := MyComputeCell{}
+	v := MyCell{}
 	v.value = onChange(source.Value())
-	v.onChange = func(int) {}
-	v.callbacks = []func(int){}
+	v.callbacks = []*func(int){}
 	switch s := source.(type) {
-	case *MyInputCell:
-		s.onChange = func(n int) {
-			s.onChange(n)
-			nd := onChange(n)
-			if nd != v.value {
-				v.value = nd
-				v.onChange(nd)
-			}
-		}
-	case *MyComputeCell:
-		s.onChange = func(n int) {
-			s.onChange(n)
-			nd := onChange(n)
-			if nd != v.value {
-				v.value = nd
-				v.onChange(nd)
-			}
-		}
+	case *MyCell:
+		s.AddCallback(func(n int) { v.SetValue(onChange(source.Value())) })
 	}
 	return &v
 }
 
 func (r *MyReactor) CreateCompute2(source1 Cell, source2 Cell, onChange func(int, int) int) ComputeCell {
-	v := MyComputeCell{}
+	v := MyCell{}
 	v.value = onChange(source1.Value(), source2.Value())
-	v.onChange = func(int) {}
-	v.callbacks = []func(int){}
+	v.callbacks = []*func(int){}
+	switch s1 := source1.(type) {
+	case *MyCell:
+		switch s2 := source2.(type) {
+		case *MyCell:
+			s1.AddCallback(func(n int) { v.SetValue(onChange(s1.Value(), s2.Value())) })
+			s2.AddCallback(func(n int) { v.SetValue(onChange(s1.Value(), s2.Value())) })
+		}
+	}
 	return &v
 }
 
 type MyCell struct {
-	value    int
-	onChange func(int)
+	value     int
+	callbacks []*func(int)
 }
 
-func (c MyCell) Value() int {
+func (c *MyCell) Value() int {
 	return c.value
 }
 
-type MyInputCell struct {
-	MyCell
+func (c *MyCell) AddCallback(callback func(int)) Canceler {
+	c.callbacks = append(c.callbacks, &callback)
+	return &MyCanceler{c, &callback}
 }
 
-func (c *MyInputCell) SetValue(v int) {
+func (c *MyCell) SetValue(v int) {
 	if c.value == v {
 		return
 	}
 	c.value = v
-	c.onChange(v)
-}
-
-type MyComputeCell struct {
-	MyCell
-	callbacks []func(int)
-}
-
-func (c *MyComputeCell) AddCallback(callback func(int)) Canceler {
-	index := len(c.callbacks)
-	c.callbacks = append(c.callbacks, callback)
-	return &MyCanceler{c, index}
+	for _, c := range c.callbacks {
+		(*c)(v)
+	}
 }
 
 type MyCanceler struct {
-	computeCell *MyComputeCell
-	index       int
+	cell     *MyCell
+	callback *func(int)
 }
 
 func (c *MyCanceler) Cancel() {
-	c.computeCell.callbacks = append(c.computeCell.callbacks[:c.index], c.computeCell.callbacks[c.index+1:]...)
+	index := -1
+	for i, cb := range c.cell.callbacks {
+		if cb == c.callback {
+			index = i
+			break
+		}
+	}
+	if index == -1 {
+		return
+	}
+	c.cell.callbacks = append(c.cell.callbacks[:index], c.cell.callbacks[index+1:]...)
 }
