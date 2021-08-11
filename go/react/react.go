3323528f1ad71b9b7d65ef1ad685e5f9d45ee1d0
Chris Pritchard <chris@grislygrotto.nz>
2021-08-11 18:33:09 +1200

    alllmost...

--- go/react/react.go
+++ go/react/react.go
@@ -17,38 +17,43 @@ func (r *MyReactor) CreateInput(n int) InputCell {
 	v := MyCell{}
 	v.value = n
 	v.callbacks = []*func(int){}
+	v.dependants = []*MyCell{}
 	return &v
 }
 
 func (r *MyReactor) CreateCompute1(source Cell, onChange func(int) int) ComputeCell {
 	v := MyCell{}
-	v.value = onChange(source.Value())
+	v.onChange = func() int { return onChange(source.Value()) }
+	v.value = v.onChange()
 	v.callbacks = []*func(int){}
 	switch s := source.(type) {
 	case *MyCell:
-		s.AddCallback(func(n int) { v.SetValue(onChange(source.Value())) })
+		s.dependants = append(s.dependants, &v)
 	}
 	return &v
 }
 
 func (r *MyReactor) CreateCompute2(source1 Cell, source2 Cell, onChange func(int, int) int) ComputeCell {
 	v := MyCell{}
-	v.value = onChange(source1.Value(), source2.Value())
+	v.onChange = func() int { return onChange(source1.Value(), source2.Value()) }
+	v.value = v.onChange()
 	v.callbacks = []*func(int){}
 	switch s1 := source1.(type) {
 	case *MyCell:
 		switch s2 := source2.(type) {
 		case *MyCell:
-			s1.AddCallback(func(n int) { v.SetValue(onChange(s1.Value(), s2.Value())) })
-			s2.AddCallback(func(n int) { v.SetValue(onChange(s1.Value(), s2.Value())) })
+			s1.dependants = append(s1.dependants, &v)
+			s2.dependants = append(s2.dependants, &v)
 		}
 	}
 	return &v
 }
 
 type MyCell struct {
-	value     int
-	callbacks []*func(int)
+	value      int
+	onChange   func() int
+	callbacks  []*func(int)
+	dependants []*MyCell
 }
 
 func (c *MyCell) Value() int {
@@ -60,14 +65,40 @@ func (c *MyCell) AddCallback(callback func(int)) Canceler {
 	return &MyCanceler{c, &callback}
 }
 
+func (c *MyCell) CallCallbacks() {
+	for _, cb := range c.callbacks {
+		(*cb)(c.value)
+	}
+}
+
 func (c *MyCell) SetValue(v int) {
 	if c.value == v {
 		return
 	}
+
 	c.value = v
-	for _, c := range c.callbacks {
-		(*c)(v)
+	c.CallCallbacks()
+
+	dirty := c.dependants
+	next := []*MyCell{}
+	for len(dirty) > 0 {
+		for _, c := range dirty {
+			dep := c.updateValue()
+			next = append(next, dep...)
+		}
+		dirty = next
+		next = []*MyCell{}
 	}
+}
+
+func (c *MyCell) updateValue() []*MyCell {
+	v := c.onChange()
+	if c.value == v {
+		return []*MyCell{}
+	}
+	c.value = v
+	c.CallCallbacks()
+	return c.dependants
 }
 
 type MyCanceler struct {
