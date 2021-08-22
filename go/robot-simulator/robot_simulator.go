e6a26af5443f929597a81e2ea4f55bee31bf551f
Chris Pritchard <chris@grislygrotto.nz>
2021-08-22 14:30:57 +1200

    finished step 2

--- go/robot-simulator/robot_simulator.go
+++ go/robot-simulator/robot_simulator.go
@@ -7,6 +7,8 @@ var E Dir = 'E'
 var S Dir = 'S'
 var W Dir = 'W'
 
+type Action byte
+
 func (d Dir) String() string {
 	return fmt.Sprintf("%c", d)
 }
@@ -48,4 +50,68 @@ func Left() {
 	case W:
 		Step1Robot.Dir = S
 	}
+}
+
+func StartRobot(cmd chan Command, act chan Action) {
+	for c := range cmd {
+		switch c {
+		case 'R':
+			act <- 'R'
+		case 'L':
+			act <- 'L'
+		case 'A':
+			act <- 'A'
+		}
+	}
+	close(act)
+}
+
+func Room(extent Rect, robot Step2Robot, act chan Action, rep chan Step2Robot) {
+	current := robot
+	for a := range act {
+		switch a {
+		case 'R':
+			switch current.Dir {
+			case N:
+				current.Dir = E
+			case S:
+				current.Dir = W
+			case E:
+				current.Dir = S
+			case W:
+				current.Dir = N
+			}
+		case 'L':
+			switch current.Dir {
+			case N:
+				current.Dir = W
+			case S:
+				current.Dir = E
+			case E:
+				current.Dir = N
+			case W:
+				current.Dir = S
+			}
+		case 'A':
+			candidate := current
+			switch candidate.Dir {
+			case N:
+				candidate.Northing++
+			case S:
+				candidate.Northing--
+			case E:
+				candidate.Easting++
+			case W:
+				candidate.Easting--
+			}
+			if candidate.Easting < extent.Min.Easting || candidate.Easting > extent.Max.Easting {
+				continue
+			}
+			if candidate.Northing < extent.Min.Northing || candidate.Northing > extent.Max.Northing {
+				continue
+			}
+			current = candidate
+		}
+	}
+	rep <- robot
 }
