d1895627650c5d3f635d7b603f969c37f3ddf956
Chris Pritchard <chris@grislygrotto.nz>
2021-09-03 10:27:45 +1200

    finished loglevels

---
+++ csharp/log-levels/LogLevels.cs
@@ -0,0 +1,12 @@
+
+static class LogLine
+{
+    public static string Message(string logLine) =>
+        logLine.Substring(logLine.IndexOf(':')+1).Trim();
+
+    public static string LogLevel(string logLine) =>
+        logLine.Substring(1, logLine.IndexOf(':')-2).ToLower();
+
+    public static string Reformat(string logLine) => 
+        $"{Message(logLine)} ({LogLevel(logLine)})";
+}
