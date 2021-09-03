0efc59029a3ca1968eb23668317c795ed26cce61
Chris Pritchard <chris@grislygrotto.nz>
2021-09-04 09:37:10 +1200

    finisherd booking for beauty

---
+++ csharp/booking-up-for-beauty/BookingUpForBeauty.cs
@@ -0,0 +1,16 @@
+using System;
+
+static class Appointment
+{
+    public static DateTime Schedule(string appointmentDateDescription) => DateTime.Parse(appointmentDateDescription);
+
+    public static bool HasPassed(DateTime appointmentDate) => appointmentDate < DateTime.Now;
+
+    public static bool IsAfternoonAppointment(DateTime appointmentDate) => 
+        appointmentDate.Hour >= 12 && appointmentDate.Hour < 18;
+
+    public static string Description(DateTime appointmentDate) =>
+        $"You have an appointment on {appointmentDate.ToString("M/d/yyyy h:mm:ss tt")}.";
+
+    public static DateTime AnniversaryDate() => new DateTime(DateTime.Now.Year, 9, 15, 0, 0, 0);
+}
