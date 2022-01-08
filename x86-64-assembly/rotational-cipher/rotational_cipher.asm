9dbc9928971b48382a581404683d8aa71e7d6de1
Chris Pritchard <chris@grislygrotto.nz>
2022-01-09 11:09:50 +1300

    finished rot cipher

--- x86-64-assembly/rotational-cipher/rotational_cipher.asm
+++ x86-64-assembly/rotational-cipher/rotational_cipher.asm
@@ -10,36 +10,36 @@ rotate:
     ; if string is upper case, rotate by A-Z
     ; if neither, just set output
 
-    mov rcx, 0      ; current string index
+    mov rcx, 0              ; current string index
 rot_char:
-    mov r8b, byte [rdi + rcx]
-    cmp r8b, 0
+    movzx r8, byte [rdi + rcx]
+    cmp r8, 0
     je finished
-    cmp r8b, 41h      ; test less than A
+    cmp r8, 41h            ; test less than A
     jl copy_to_buffer
-    cmp r8b, 5Ah      ; test less than or equal to Z
+    cmp r8, 5Ah            ; test less than or equal to Z
     jle rot_uppercase
-    cmp r8b, 61h      ; test less than a
+    cmp r8, 61h            ; test less than a
     jl copy_to_buffer
-    cmp r8b, 7Ah     ; test less than or equal to z
+    cmp r8, 7Ah            ; test less than or equal to z
     jle rot_lowercase
     jmp copy_to_buffer
 rot_uppercase:
-    add r8b, sil
-    cmp r8b, 5Ah
+    add r8, rsi            ; bottom 8 bytes of rsi
+    cmp r8, 5Ah
     jle copy_to_buffer
-    sub r8b, 41h
+    sub r8, 26             ; rotate around
     jmp copy_to_buffer
 rot_lowercase:
-    add r8b, sil
-    cmp r8b, 7Ah
+    add r8, rsi            ; bottom 8 bytes of rsi
+    cmp r8, 7Ah
     jle copy_to_buffer
-    sub r8b, 61h
+    sub r8, 26             ; rotate around
     jmp copy_to_buffer
 copy_to_buffer:
     mov byte [rdx + rcx], r8b
     inc rcx
     jmp rot_char
 finished:
-    mov byte [rdx + rcx], 0
+    mov byte [rdx + rcx], 0 ; end the string in the buffer
     ret
